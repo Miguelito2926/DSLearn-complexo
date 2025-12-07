9776b3b4cd77afd206b8449bee8fbb29cb77ab29
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-12-07 10:45:17 -0300

    Relacionamento entre section e resource

--- src/main/java/com/devsuperior/dslearn/entities/Resource.java
+++ src/main/java/com/devsuperior/dslearn/entities/Resource.java
@@ -7,11 +7,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -34,4 +38,7 @@ public class Resource {
     @ManyToOne
     @JoinColumn(name = "offer_id")
     private Offer offer;
+
+    @OneToMany(mappedBy = "section_id")
+    private List<Section> sections = new ArrayList<>();
 }
