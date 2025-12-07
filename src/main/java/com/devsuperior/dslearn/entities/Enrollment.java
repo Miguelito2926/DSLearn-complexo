76467341a1485e61eb0c84f5b128c39aedde413f
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-12-07 12:58:29 -0300

    Configurando relacionamento entre User e Offer com criação das tabelas de associação Enrollment e EnrollmentPK

---
+++ src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
@@ -0,0 +1,58 @@
+package com.devsuperior.dslearn.entities;
+
+import com.devsuperior.dslearn.pk.EnrollmentPK;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant refundMoment;
+    private boolean available;
+    private boolean onlyUpdate;
+
+    public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available, boolean onlyUpdate) {
+        id.setUser(user);
+        id.setOffer(offer);
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.available = available;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User user) {
+        id.setUser(user);
+    }
+
+    public Offer getOffer() {
+        return id.getOffer();
+    }
+
+    public void setOffert(Offer offer) {
+        id.setOffer(offer);
+    }
+}
+
+
