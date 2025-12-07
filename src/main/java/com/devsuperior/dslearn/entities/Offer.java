680adc84ecca86307e0bf22de6d132806e17d1b8
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-12-07 13:35:07 -0300

    Configurando colunas da tabela offer com timezone

--- src/main/java/com/devsuperior/dslearn/entities/Offer.java
+++ src/main/java/com/devsuperior/dslearn/entities/Offer.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslearn.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,7 +30,11 @@ public class Offer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String edition;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant startMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant endMoment;
 
     @ManyToOne
